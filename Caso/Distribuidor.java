c3cd08f4ffcc6edbb3e0c1ddd4da1eaa3a1edae5
danielfvargas <123401702+danielfvargas@users.noreply.github.com>
2024-09-08 15:10:23 -0500

    proceso de distribuidor (comentarios)

--- Caso/Distribuidor.java
+++ Caso/Distribuidor.java
@@ -17,20 +17,20 @@ class Distribuidor extends Thread {
             while (!finaliza) {
                 synchronized (buffer) {
                     if (buffer.productos.isEmpty()) {
-                        buffer.wait(); 
+                        buffer.wait(); // Espera si el buffer está vacío
                     } else {
-                        Producto primerProducto = buffer.productos.peek();
+                        Producto primerProducto = buffer.productos.peek(); // Obtiene el primer producto del buffer
                         if ((tipoDistribuidor == Producto.Tipo.A && (primerProducto.getTipo() == Producto.Tipo.A || primerProducto.getTipo() == Producto.Tipo.FIN_A)) ||
                             (tipoDistribuidor == Producto.Tipo.B && (primerProducto.getTipo() == Producto.Tipo.B || primerProducto.getTipo() == Producto.Tipo.FIN_B))) {
                             
-                            Producto producto = buffer.retirar(); 
-                            buffer.notifyAll(); 
+                            Producto producto = buffer.retirar();  // Retira el producto del buffer
+                            buffer.notifyAll(); // Notifica a los hilos en espera
     
                             if (producto.getTipo() == Producto.Tipo.FIN_A || producto.getTipo() == Producto.Tipo.FIN_B) {
-                                finaliza = true;
+                                finaliza = true; // Finaliza si se retira el producot final
                             }
                         } else {
-                            buffer.wait();
+                            buffer.wait(); // Espera si el producto no es del tipo del distribuidor
                         }
                     }
                 }
